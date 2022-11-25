b517d5db8b819298f04e8bbce2e4af4c85bec737
Guilherme Wanderley Espinola <gwanderleyespinola@gmail.com>
2022-11-25 13:58:43 -0300

    adicionada a anotaÃcao @Repository

--- src/main/java/io/github/luaprogrammer/lojavirtual/repositories/PessoaRepository.java
+++ src/main/java/io/github/luaprogrammer/lojavirtual/repositories/PessoaRepository.java
@@ -2,7 +2,9 @@ package io.github.luaprogrammer.lojavirtual.repositories;
 
 import io.github.luaprogrammer.lojavirtual.entities.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
 
 }
